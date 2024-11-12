84e2a107ab9f26150953d4a07db2976f2ac68b83
Deep25-1975 <deep25.1975@gmail.com>
2024-11-12 14:16:13 +0530

    added two lines

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, 123" << std::endl;
+    std::cout << "git version" << std::endl;
+    return 0;
+}
